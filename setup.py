9a40a6fd44c14f884831cf8982a8e963c525a33e
Cément <pene.clement@gmail.com>
2022-12-26 13:20:04 +0100

    Initial commit + robot node control with basic String command

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'sii_bot'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='clem-irwt',
+    maintainer_email='clement.pene@sii.fr',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+		'motors_control = sii_bot.motors:main',
+        ],
+    },
+)
---
+++ sii_bot/motors.py
@@ -0,0 +1,140 @@
+# Contain motors node to control the wheels
+# TODO : subscribe to command_vel
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+import RPi.GPIO as GPIO  # Import the GPIO Library
+import time  # Import the Time library
+
+# Set the GPIO modes
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+# Set variables for the GPIO motor pins
+pinMotorAForwards = 10
+pinMotorABackwards = 9
+pinMotorBForwards = 8
+pinMotorBBackwards = 7
+
+# How many times to turn the pin on and off each second
+Frequency = 20
+# How long the pin stays on each cycle, as a percent (here, it's 30%)
+DutyCycle = 30
+# Setting the duty cycle to 0 means the motors will not turn
+Stop = 0
+
+# Set the GPIO Pin mode to be Output
+GPIO.setup(pinMotorAForwards, GPIO.OUT)
+GPIO.setup(pinMotorABackwards, GPIO.OUT)
+GPIO.setup(pinMotorBForwards, GPIO.OUT)
+GPIO.setup(pinMotorBBackwards, GPIO.OUT)
+
+# Set the GPIO to software PWM at 'Frequency' Hertz
+pwmMotorAForwards = GPIO.PWM(pinMotorAForwards, Frequency)
+pwmMotorABackwards = GPIO.PWM(pinMotorABackwards, Frequency)
+pwmMotorBForwards = GPIO.PWM(pinMotorBForwards, Frequency)
+pwmMotorBBackwards = GPIO.PWM(pinMotorBBackwards, Frequency)
+
+# Start the software PWM with a duty cycle of 0 (i.e. not moving)
+pwmMotorAForwards.start(Stop)
+pwmMotorABackwards.start(Stop)
+pwmMotorBForwards.start(Stop)
+pwmMotorBBackwards.start(Stop)
+
+
+# Turn all motors off
+def stopmotors():
+    pwmMotorAForwards.ChangeDutyCycle(Stop)
+    pwmMotorABackwards.ChangeDutyCycle(Stop)
+    pwmMotorBForwards.ChangeDutyCycle(Stop)
+    pwmMotorBBackwards.ChangeDutyCycle(Stop)
+
+
+# Turn both motors forwards
+def forwards():
+    pwmMotorAForwards.ChangeDutyCycle(DutyCycle)
+    pwmMotorABackwards.ChangeDutyCycle(Stop)
+    pwmMotorBForwards.ChangeDutyCycle(DutyCycle)
+    pwmMotorBBackwards.ChangeDutyCycle(Stop)
+
+
+# Turn both motors backwards
+def backwards():
+    pwmMotorAForwards.ChangeDutyCycle(Stop)
+    pwmMotorABackwards.ChangeDutyCycle(DutyCycle)
+    pwmMotorBForwards.ChangeDutyCycle(Stop)
+    pwmMotorBBackwards.ChangeDutyCycle(DutyCycle)
+
+
+# Turn left
+def left():
+    pwmMotorAForwards.ChangeDutyCycle(Stop)
+    pwmMotorABackwards.ChangeDutyCycle(DutyCycle)
+    pwmMotorBForwards.ChangeDutyCycle(DutyCycle)
+    pwmMotorBBackwards.ChangeDutyCycle(Stop)
+
+
+# Turn Right
+def right():
+    pwmMotorAForwards.ChangeDutyCycle(DutyCycle)
+    pwmMotorABackwards.ChangeDutyCycle(Stop)
+    pwmMotorBForwards.ChangeDutyCycle(Stop)
+    pwmMotorBBackwards.ChangeDutyCycle(DutyCycle)
+
+
+class MotorsControl(Node):
+
+    def __init__(self):
+        super().__init__('motor_control_sub')
+        self.subscription = self.create_subscription(
+            String,
+            'motor_command',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
+        if msg.data == 'forward':
+            print('Moving Forwards')
+            forwards()
+        elif msg.data == 'backward':
+            print('Moving Backward')
+            backwards()
+        elif msg.data == 'left':
+            print('Moving to the left')
+            left()
+        elif msg.data == 'right':
+            print('Moving Forwards')
+            right()
+        else :
+            print('unknown command, stoping instead')
+            stopmotors()
+
+
+def main(args=None):
+
+
+
+    rclpy.init(args=args)
+
+    motors_node = MotorsControl()
+
+    rclpy.spin(motors_node)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    motors_node.destroy_node()
+    rclpy.shutdown()
+    stopmotors()
+    GPIO.cleanup()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
