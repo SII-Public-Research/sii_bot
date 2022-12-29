b639edd504c013b7005ec976057c18499f1986a9
Clement <clement.pene@sii.fr>
2022-12-29 10:24:38 +0100

    testing following line

--- sii_bot/line_follower.py
+++ sii_bot/line_follower.py
@@ -67,24 +67,24 @@ class LineFollower(rclpy.node.Node):
                     self._angular_velocity = 0.0
                 case (0, 0, 1):
                     print('Je derive a fond vers la gauche, a droite toute !')
-                    self._linear_velocity = 1.0
-                    self._angular_velocity = 1.5
+                    self._linear_velocity = 0.5
+                    self._angular_velocity = 1.0
                 case (0, 1, 1):
                     print('Je derive un peu vers la gauche, a droite !')
-                    self._linear_velocity = 1.0
-                    self._angular_velocity = 1.0
+                    self._linear_velocity = 0.5
+                    self._angular_velocity = 0.5
                 case (1, 1, 1):
                     print('On va tout droit !')
-                    self._linear_velocity = 1.5
+                    self._linear_velocity = 0.5
                     self._angular_velocity = 0.0
                 case (1, 1, 0):
                     print('Je derive un peu vers la droite, a gauche !')
-                    self._linear_velocity = 1.0
-                    self._angular_velocity = -1.0
+                    self._linear_velocity = 0.5
+                    self._angular_velocity = -0.5
                 case (1, 0, 0):
                     print('Je derive a fond vers la droite, a gauche toute ! ')
-                    self._linear_velocity = 1.0
-                    self._angular_velocity = -1.5
+                    self._linear_velocity = 0.5
+                    self._angular_velocity = -1.0
                 case _:
                     print('Cas bizarre, je ne sais pas quoi faire !')
                     self._linear_velocity = 0.0
